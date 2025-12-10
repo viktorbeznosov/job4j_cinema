d576d4d2d2d8eb84de10d6f945d48d2cfb2b1305
viktorbeznosov <viktorbeznosov@mail.ru>
2025-12-10 21:44:00 +0300

    Cinema - add IndexController

---
+++ src/main/java/ru/job4j/cinema/controller/IndexController.java
@@ -0,0 +1,19 @@
+package ru.job4j.cinema.controller;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@ThreadSafe
+@Controller
+@SuppressWarnings("unused")
+public class IndexController {
+    @GetMapping({"/", "/index"})
+    public String getIndex(Model model, HttpSession session) {
+        return "index";
+    }
+}
